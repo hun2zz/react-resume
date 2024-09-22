ea362e2fc635a8bfb0b72de79c161b4d131d9300
hun2zz <tkdgnsdldkdlel@naver.com>
2024-09-22 17:05:56 +0900

    :white_check_mark: 수정

--- src/components/pages/MainPages.jsx
+++ src/components/pages/MainPages.jsx
@@ -1,32 +1,57 @@
+import React, { useState, useEffect } from "react";
 import {
-  Fullpage,
-  FullpageCount,
-  FullpageNavigation,
-  FullpageNumber,
-  FullpageSection,
-  FullPageSections,
-} from "@ap.cx/react-fullpage";
-import React, { useState } from "react";
-import styles from "./MainPages.module.scss";
+  Element,
+  Events,
+  animateScroll as scroll,
+  scroller,
+} from "react-scroll";
 import Intro from "./Intro";
 import NavigationBar from "./NavigationBar";
 import ScrollIndicator from "./ScrollIndicator";
 import Introduce from "./Introduce";
-import LogCurrentSection from "./LogCurrentSection";
 import Skill from "./Skill";
 import Project from "./Project";
 
 const MainPages = () => {
-  const [currentSection, setCurrentSection] = useState(0);
+  useEffect(() => {
+    Events.scrollEvent.register("begin", function () {
+      console.log("begin", arguments);
+    });
+
+    Events.scrollEvent.register("end", function () {
+      console.log("end", arguments);
+    });
+
+    return () => {
+      Events.scrollEvent.remove("begin");
+      Events.scrollEvent.remove("end");
+    };
+  }, []);
+
   return (
     <>
-      <NavigationBar currentSection={currentSection}></NavigationBar>
+      <NavigationBar />
       <ScrollIndicator />
-      <Intro />
-      <Introduce></Introduce>
-      <Skill></Skill>
-      <Project />
-      <h1>Screen 5</h1>
+
+      <Element name="intro" className="element">
+        <Intro />
+      </Element>
+
+      <Element name="introduce" className="element">
+        <Introduce />
+      </Element>
+
+      <Element name="skill" className="element">
+        <Skill />
+      </Element>
+
+      <Element name="project" className="element">
+        <Project />
+      </Element>
+
+      <Element name="screen5" className="element">
+        <h1>Screen 5</h1>
+      </Element>
     </>
   );
 };
