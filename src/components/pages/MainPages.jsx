b4e701506629f789c7a856948393f957ae65c020
hun2zz <tkdgnsdldkdlel@naver.com>
2024-09-24 17:56:37 +0900

    :white_check_mark: 수정

--- src/components/pages/MainPages.jsx
+++ src/components/pages/MainPages.jsx
@@ -1,6 +1,8 @@
-import React, { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { Element } from "react-scroll";
+import LocomotiveScroll from "locomotive-scroll";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 import Intro from "./Intro";
 import NavigationBar from "./NavigationBar";
 import Introduce from "./Introduce";
@@ -10,61 +12,70 @@ import styles from "./MainPages.module.scss";
 import MarqueeComponent from "./MarqueeComponent";
 
 const MainPages = () => {
-  const introduceRef = useRef(null);
+  const containerRef = useRef(null);
+  const scrollY = useMotionValue(0);
 
-  const { scrollYProgress } = useScroll({
-    target: introduceRef,
-    offset: ["start end", "end start"],
-  });
-
-  const yellowOpacity = useTransform(
-    scrollYProgress,
-    [0.2, 0.4, 0.5, 0.6], // 조정된 스크롤 진행 지점
-    // [
-    //   "rgba(0, 0, 0, 1)", // 시작 시 검은색
-    //   "rgba(0, 0, 0, 1)", // 약간 늦춰진 시작점
-    //   "rgba(238, 255, 4, 1)", // Introduce 섹션 내부 노란색
-    //   "rgba(0, 0, 0, 1)", // 더 빨리 검은색으로 돌아감
-    // ]
-    [0, 1, 1, 0] // 투명도 설정 (노란색 레이어)
+  const backgroundColor = useTransform(
+    scrollY,
+    [
+      0,
+      window.innerHeight * 0.8, // Start transition earlier
+      window.innerHeight * 1.2, // Complete transition later
+      window.innerHeight * 1.75, // Start reverting earlier
+      window.innerHeight * 2, // Complete reversion later
+    ],
+    ["#222", "#222", "#eeff04", "#eeff04", "#222"]
   );
 
-  return (
-    <motion.main>
-      <NavigationBar />
-      {/* 노란색 배경 레이어 */}
-      <motion.div
-        className="yellowBackground"
-        style={{
-          opacity: yellowOpacity, // 스크롤에 따른 투명도 설정
-        }}
-      />
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: containerRef.current,
+      smooth: true,
+      multiplier: 0.4, // 0.6으로 감소, 스크롤 속도를 더 느리게 함
+      lerp: 0.08, // 0.08로 설정, 부드러운 전환 유지
+      smartphone: {
+        smooth: true,
+        multiplier: 0.6,
+      },
+      tablet: {
+        smooth: true,
+        multiplier: 0.6,
+      },
+    });
 
+    scroll.on("scroll", (instance) => {
+      scrollY.set(instance.scroll.y);
+    });
+
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, [scrollY]);
+
+  return (
+    <motion.main ref={containerRef} style={{ backgroundColor }}>
+      <NavigationBar />
       <div data-section="intro">
         <Element name="intro" className={styles.section}>
           <Intro />
           <MarqueeComponent />
         </Element>
       </div>
-
-      <div ref={introduceRef} data-section="introduce">
+      <div data-section="introduce">
         <Element name="introduce" className={styles.section}>
           <Introduce />
         </Element>
       </div>
-
       <div data-section="skill">
         <Element name="skill" className={styles.section}>
           <Skill />
         </Element>
       </div>
-
       <div data-section="project">
         <Element name="project" className={styles.section}>
           <Project />
         </Element>
       </div>
-
       <div data-section="screen5">
         <Element name="screen5" className={styles.section}>
           <h1>Screen 5</h1>
