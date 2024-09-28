5073f5cdbe3bc715b8130dc85542d3d7c1e29ecf
hun2zz <tkdgnsdldkdlel@naver.com>
2024-09-28 22:42:34 +0900

    :white_check_mark: 스킬부분 추가

--- src/components/pages/Skill.jsx
+++ src/components/pages/Skill.jsx
@@ -12,15 +12,35 @@ const Skill = () => {
     {
       category: "BackEnd",
       technologies: [
-        "Java", "Groovy", "NodeJS", "Spring Web", "Spring Boot", "Spring MVC",
-        "Spring Security", "Spring Data JPA", "Querydsl", "MyBatis", "Redis",
-        "JWT(JSON Web Token)", "WebSocket", "Hibernate", "Junit4/5", "Gradle", "Maven",
+        "Java",
+        "Groovy",
+        "NodeJS",
+        "Spring Web",
+        "Spring Boot",
+        "Spring MVC",
+        "Spring Security",
+        "Spring Data JPA",
+        "Querydsl",
+        "MyBatis",
+        "Redis",
+        "JWT(JSON Web Token)",
+        "WebSocket",
+        "Hibernate",
+        "Junit4/5",
+        "Gradle",
+        "Maven",
       ],
     },
     {
       category: "DevOps",
       technologies: [
-        "MySQL", "MariaDB", "GithubAction", "AWS RDS", "AWS S3", "AWS EC2", "Docker",
+        "MySQL",
+        "MariaDB",
+        "GithubAction",
+        "AWS RDS",
+        "AWS S3",
+        "AWS EC2",
+        "Docker",
       ],
     },
     {
@@ -30,20 +50,36 @@ const Skill = () => {
   ];
 
   useEffect(() => {
+    const triggers = [];
+
     categoryRefs.current.forEach((category, index) => {
-      const direction = index % 2 === 0 ? -1 : 1;
-      
-      gsap.to(category.querySelector(`.${styles.categoryInner}`), {
-        xPercent: direction * 100,
-        ease: "none",
+      const inner = category.querySelector(`.${styles.categoryInner}`);
+      const direction = index % 2 === 0 ? -1 : 1; // 짝수 인덱스는 왼쪽, 홀수 인덱스는 오른쪽으로 이동
+
+      // 모든 카테고리에 대해 동일한 초기 위치 설정
+      gsap.set(inner, { xPercent: direction * 30 });
+
+      const tl = gsap.timeline({
         scrollTrigger: {
           trigger: skillSectionRef.current,
           start: "top bottom",
           end: "bottom top",
-          scrub: 1,
+          scrub: 2,
         },
       });
+
+      tl.to(inner, {
+        xPercent: direction * -30, // 반대 방향으로 30% 이동
+        ease: "none",
+        duration: 5,
+      });
+
+      triggers.push(tl.scrollTrigger);
     });
+
+    return () => {
+      triggers.forEach((trigger) => trigger.kill());
+    };
   }, []);
 
   return (
@@ -59,14 +95,23 @@ const Skill = () => {
       <div className={styles.infoSection}>
         {skills.map((skillCategory, index) => (
           <div key={index} className={styles.column}>
-            <div className={styles.categoryWrapper} ref={(el) => (categoryRefs.current[index] = el)}>
-              <div className={styles.categoryInner}>
-                {Array(20).fill(null).map((_, i) => (
-                  <React.Fragment key={i}>
-                    <span>{skillCategory.category}</span>
-                    <div className={styles.circle}></div>
-                  </React.Fragment>
-                ))}
+            <div
+              className={styles.categoryWrapper}
+              ref={(el) => (categoryRefs.current[index] = el)}
+            >
+              <div
+                className={`${styles.categoryInner} ${
+                  skillCategory.category === "DevOps" ? styles.reverse : ""
+                }`}
+              >
+                {Array(40)
+                  .fill(null)
+                  .map((_, i) => (
+                    <React.Fragment key={i}>
+                      <span>{skillCategory.category}</span>
+                      <div className={styles.circle}></div>
+                    </React.Fragment>
+                  ))}
               </div>
             </div>
             <div className={styles.skillListWrapper}>
